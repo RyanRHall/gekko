8903f7ccf8acdc21d747a25f5e049732d492bf93
ryan_aws_gekko <hall.ryan.r+ryan_aws_gekko@gmail.com>
2018-01-06 17:11:20 -0600

    deploy_config

---
+++ bin/connect.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+ssh -i ~/.ssh/AWSGekko.pem ec2-user@54.245.44.243
--- web/vue/UIconfig.js
+++ web/vue/UIconfig.js
@@ -4,16 +4,16 @@
 // see https://github.com/askmike/gekko/blob/stable/docs/installing_gekko_on_a_server.md
 
 const CONFIG = {
-  headless: false,
+  headless: true,
   api: {
     host: '127.0.0.1',
-    port: 80,
+    port: 3000,
     timeout: 120000 // 2 minutes
   },
   ui: {
-    ssl: false,
+    ssl: true,
     host: 'localhost',
-    port: 80,
+    port: 443,
     path: '/'
   },
   adapter: 'sqlite'
